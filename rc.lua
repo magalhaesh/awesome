f7d08a31caace91cc14e5ef04d75b402cb5aad4e
Henrique Magalhães <henrique.albertino@gmail.com>
2013-08-28 20:47:17 -0300

    Commenting out parts that won't work on Symbiont.

--- rc.lua
+++ rc.lua
@@ -5,7 +5,7 @@ awful.rules = require("awful.rules")
 require("awful.autofocus")
 -- Widget and layout library
 local wibox = require("wibox")
-local volume = require("volume")
+--local volume = require("volume")
 -- Theme handling library
 local beautiful = require("beautiful")
 -- Notification library
@@ -191,7 +191,7 @@ for s = 1, screen.count() do
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
     if s == 1 then right_layout:add(wibox.widget.systray()) end
-    right_layout:add(volume_widget)
+    -- right_layout:add(volume_widget)
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
