add1409f767a84023b9a068bd00829a7dd5888b9
Henrique Magalhães <henrique.albertino@gmail.com>
2013-10-14 09:45:12 -0300

    Changed urgent window color again.

--- themes/default/theme.lua
+++ themes/default/theme.lua
@@ -8,7 +8,7 @@ theme.font          = "sans 8"
 
 theme.bg_normal     = "#222222"
 theme.bg_focus      = "#535d6c"
-theme.bg_urgent     = "#375988"
+theme.bg_urgent     = "#009ad7"
 theme.bg_minimize   = "#444444"
 theme.bg_systray    = theme.bg_normal
 
