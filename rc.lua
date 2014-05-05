9fb397dea6510d7a6d614d25b3a1f628332d5f91
Henrique Magalhães <henrique.albertino@gmail.com>
2014-05-05 18:59:04 -0300

    Putting monitor widgets on one screen only.

--- rc.lua
+++ rc.lua
@@ -206,10 +206,12 @@ for s = 1, screen.count() do
 
     -- Widgets that are aligned to the right
     local right_layout = wibox.layout.fixed.horizontal()
-    right_layout:add(cpuwidget)
-    right_layout:add(memwidget)
-    right_layout:add(diowidget)
-    if s == 1 then right_layout:add(wibox.widget.systray()) end
+    if s == 1 then 
+        right_layout:add(cpuwidget)
+        right_layout:add(memwidget)
+        right_layout:add(diowidget)
+        right_layout:add(wibox.widget.systray())
+    end
     right_layout:add(mytextclock)
     right_layout:add(mylayoutbox[s])
 
