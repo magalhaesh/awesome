75a6e8c95b6cb777bd2467353307499094b8d960
Henrique Magalhães <henrique.albertino@gmail.com>
2016-07-02 01:20:57 -0300

    Changing Wallpaper.

--- rc.lua
+++ rc.lua
@@ -76,9 +76,9 @@ local layouts = {
 
 -- {{{ Wallpaper
 if beautiful.wallpaper then
-    -- for s = 1, screen.count() do
+    for s = 1, screen.count() do
         gears.wallpaper.maximized(beautiful.wallpaper, s, false)
-    -- end
+    end
 end
 -- }}}
 
