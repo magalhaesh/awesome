3b7fbeaa338fbc9fb19c6e75a5906d143df9db91
Henrique Magalhães <henrique.albertino@gmail.com>
2019-08-16 15:57:15 -0300

    Removing rounded borders during fullscreen

--- rc.lua
+++ rc.lua
@@ -671,6 +671,14 @@ client.connect_signal("mouse::enter", function(c)
     end
 end)
 
+client.connect_signal("property::fullscreen", function(c)
+    if c.fullscreen then
+        c.shape = gears.shape.rectangle
+    else
+        c.shape = gears.shape.rounded_rect
+    end
+end)
+
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
