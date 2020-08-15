1193b7edc610bb9e32d257666f397af2d16db0cd
Henrique Magalhães <henrique.albertino@gmail.com>
2020-08-15 13:20:19 -0300

    Minor refactor on theme code

--- themes/wayfarer/theme.lua
+++ themes/wayfarer/theme.lua
@@ -1,35 +1,37 @@
 ---------------------------
--- Modified Default awesome theme (Glazunov) --
+-- Modified Default awesome theme --
 ---------------------------
 
+local theme_name = "wayfarer"
 local theme_assets = require("beautiful.theme_assets")
 local xresources = require("beautiful.xresources")
 local dpi = xresources.apply_dpi
 
 local themes_path = "~/.config/awesome/themes/"
+local theme_path = themes_path .. theme_name .. "/"
 
 local theme = {}
 
-theme.font          = "Hack 8"
+theme.font = "DejaVu Sans Code 8"
 
-theme.bg_normal     = "#282a36"
-theme.bg_focus      = "#9474d4"
-theme.bg_urgent     = "#5236C4"
-theme.bg_minimize   = "#000000"
-theme.bg_systray    = theme.bg_normal
+theme.bg_normal = "#282a36"
+theme.bg_focus = "#9474d4"
+theme.bg_urgent = "#5236C4"
+theme.bg_minimize = "#000000"
+theme.bg_systray = theme.bg_normal
 
-theme.fg_normal     = "#aaaaaa"
-theme.fg_focus      = "#000000"
-theme.fg_urgent     = "#ffffff"
-theme.fg_minimize   = "#ffffff"
+theme.fg_normal = "#aaaaaa"
+theme.fg_focus = "#000000"
+theme.fg_urgent = "#ffffff"
+theme.fg_minimize = "#ffffff"
 
-theme.widget_fg_normal   = "#fefefe"
-
-theme.useless_gap   = dpi(2)
-theme.border_width  = dpi(2)
+theme.border_width = dpi(2)
 theme.border_normal = "#1c2022"
-theme.border_focus  = "#8e71b7"
+theme.border_focus = "#8e71b7"
 theme.border_marked = "#3ca4d8"
+
+theme.widget_fg_normal = "#fefefe"
+theme.useless_gap = dpi(2)
 
 -- There are other variable sets
 -- overriding the default one when
@@ -62,7 +64,7 @@ theme.taglist_squares_unsel = theme_assets.taglist_squares_unsel(
 -- Variables set for theming the menu:
 -- menu_[bg|fg]_[normal|focus]
 -- menu_[border_color|border_width]
-theme.menu_submenu_icon = themes_path.."default/submenu.png"
+theme.menu_submenu_icon = themes_path .. "default/submenu.png"
 theme.menu_height = dpi(10)
 theme.menu_width  = dpi(100)
 
@@ -71,72 +73,78 @@ theme.menu_width  = dpi(100)
 -- beautiful.variable in your rc.lua
 --theme.bg_widget = "#cc0000"
 
--- Define the image to load
-theme.titlebar_close_button_normal = themes_path.."default/titlebar/close_normal.png"
-theme.titlebar_close_button_focus  = themes_path.."default/titlebar/close_focus.png"
+-- Titlebar icons. Not really used on this theme, but who knows.
+local titlebar_icon_path = themes_path .. "default/titlebar/"
 
-theme.titlebar_minimize_button_normal = themes_path.."default/titlebar/minimize_normal.png"
-theme.titlebar_minimize_button_focus  = themes_path.."default/titlebar/minimize_focus.png"
+theme.titlebar_close_button_normal = titlebar_icon_path .. "close_normal.png"
+theme.titlebar_close_button_focus  = titlebar_icon_path .. "close_focus.png"
 
-theme.titlebar_ontop_button_normal_inactive = themes_path.."default/titlebar/ontop_normal_inactive.png"
-theme.titlebar_ontop_button_focus_inactive  = themes_path.."default/titlebar/ontop_focus_inactive.png"
-theme.titlebar_ontop_button_normal_active = themes_path.."default/titlebar/ontop_normal_active.png"
-theme.titlebar_ontop_button_focus_active  = themes_path.."default/titlebar/ontop_focus_active.png"
+theme.titlebar_minimize_button_normal = titlebar_icon_path .. "minimize_normal.png"
+theme.titlebar_minimize_button_focus  = titlebar_icon_path .. "minimize_focus.png"
 
-theme.titlebar_sticky_button_normal_inactive = themes_path.."default/titlebar/sticky_normal_inactive.png"
-theme.titlebar_sticky_button_focus_inactive  = themes_path.."default/titlebar/sticky_focus_inactive.png"
-theme.titlebar_sticky_button_normal_active = themes_path.."default/titlebar/sticky_normal_active.png"
-theme.titlebar_sticky_button_focus_active  = themes_path.."default/titlebar/sticky_focus_active.png"
+theme.titlebar_ontop_button_normal_inactive = titlebar_icon_path .. "ontop_normal_inactive.png"
+theme.titlebar_ontop_button_focus_inactive  = titlebar_icon_path .. "ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_active = titlebar_icon_path .. "ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_active  = titlebar_icon_path .. "ontop_focus_active.png"
 
-theme.titlebar_floating_button_normal_inactive = themes_path.."default/titlebar/floating_normal_inactive.png"
-theme.titlebar_floating_button_focus_inactive  = themes_path.."default/titlebar/floating_focus_inactive.png"
-theme.titlebar_floating_button_normal_active = themes_path.."default/titlebar/floating_normal_active.png"
-theme.titlebar_floating_button_focus_active  = themes_path.."default/titlebar/floating_focus_active.png"
+theme.titlebar_sticky_button_normal_inactive = titlebar_icon_path .. "sticky_normal_inactive.png"
+theme.titlebar_sticky_button_focus_inactive  = titlebar_icon_path .. "sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_active = titlebar_icon_path .. "sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_active  = titlebar_icon_path .. "sticky_focus_active.png"
 
-theme.titlebar_maximized_button_normal_inactive = themes_path.."default/titlebar/maximized_normal_inactive.png"
-theme.titlebar_maximized_button_focus_inactive  = themes_path.."default/titlebar/maximized_focus_inactive.png"
-theme.titlebar_maximized_button_normal_active = themes_path.."default/titlebar/maximized_normal_active.png"
-theme.titlebar_maximized_button_focus_active  = themes_path.."default/titlebar/maximized_focus_active.png"
+theme.titlebar_floating_button_normal_inactive = titlebar_icon_path .. "floating_normal_inactive.png"
+theme.titlebar_floating_button_focus_inactive  = titlebar_icon_path .. "floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_active = titlebar_icon_path .. "floating_normal_active.png"
+theme.titlebar_floating_button_focus_active  = titlebar_icon_path .. "floating_focus_active.png"
 
-theme.wallpaper = themes_path.."glazunov/background.png"
+theme.titlebar_maximized_button_normal_inactive = titlebar_icon_path .. "maximized_normal_inactive.png"
+theme.titlebar_maximized_button_focus_inactive  = titlebar_icon_path .. "maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_active = titlebar_icon_path .. "maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_active  = titlebar_icon_path .. "maximized_focus_active.png"
 
--- You can use your own layout icons like this:
-theme.layout_fairh = themes_path.."default/layouts/fairhw.png"
-theme.layout_fairv = themes_path.."default/layouts/fairvw.png"
-theme.layout_floating  = themes_path.."default/layouts/floatingw.png"
-theme.layout_magnifier = themes_path.."default/layouts/magnifierw.png"
-theme.layout_max = themes_path.."default/layouts/maxw.png"
-theme.layout_fullscreen = themes_path.."default/layouts/fullscreenw.png"
-theme.layout_tilebottom = themes_path.."default/layouts/tilebottomw.png"
-theme.layout_tileleft   = themes_path.."default/layouts/tileleftw.png"
-theme.layout_tile = themes_path.."default/layouts/tilew.png"
-theme.layout_tiletop = themes_path.."default/layouts/tiletopw.png"
-theme.layout_spiral  = themes_path.."default/layouts/spiralw.png"
-theme.layout_dwindle = themes_path.."default/layouts/dwindlew.png"
-theme.layout_cornernw = themes_path.."default/layouts/cornernww.png"
-theme.layout_cornerne = themes_path.."default/layouts/cornernew.png"
-theme.layout_cornersw = themes_path.."default/layouts/cornersww.png"
-theme.layout_cornerse = themes_path.."default/layouts/cornersew.png"
+theme.wallpaper = theme_path .. "background.png"
 
--- Custom icons
-theme.widget_ac = themes_path .. "glazunov/icons/ac.png"
-theme.widget_battery = themes_path .. "glazunov/icons/battery.png"
-theme.widget_battery_low = themes_path .. "glazunov/icons/battery_low.png"
-theme.widget_battery_empty = themes_path .. "glazunov/icons/battery_empty.png"
-theme.widget_mem = themes_path .. "glazunov/icons/mem.png"
-theme.widget_cpu = themes_path .. "glazunov/icons/cpu.png"
-theme.widget_temp = themes_path .. "glazunov/icons/temp.png"
-theme.widget_net = themes_path .. "glazunov/icons/net.png"
-theme.widget_hdd = themes_path .. "glazunov/icons/hdd.png"
-theme.widget_music = themes_path .. "glazunov/icons/note.png"
-theme.widget_music_on = themes_path .. "glazunov/icons/note_on.png"
-theme.widget_vol = themes_path .. "glazunov/icons/vol.png"
-theme.widget_vol_low = themes_path .. "glazunov/icons/vol_low.png"
-theme.widget_vol_no = themes_path .. "glazunov/icons/vol_no.png"
-theme.widget_vol_mute = themes_path .. "glazunov/icons/vol_mute.png"
-theme.widget_mail = themes_path .. "glazunov/icons/mail.png"
-theme.widget_mail_on = themes_path .. "glazunov/icons/mail_on.png"
-theme.widget_task = themes_path .. "glazunov/icons/task.png"
+-- Layout icons.
+local layout_icon_path = themes_path .. "default/layouts/"
+
+theme.layout_fairh = layout_icon_path .. "fairhw.png"
+theme.layout_fairv = layout_icon_path .. "fairvw.png"
+theme.layout_floating  = layout_icon_path .. "floatingw.png"
+theme.layout_magnifier = layout_icon_path .. "magnifierw.png"
+theme.layout_max = layout_icon_path .. "maxw.png"
+theme.layout_fullscreen = layout_icon_path .. "fullscreenw.png"
+theme.layout_tilebottom = layout_icon_path .. "tilebottomw.png"
+theme.layout_tileleft   = layout_icon_path .. "tileleftw.png"
+theme.layout_tile = layout_icon_path .. "tilew.png"
+theme.layout_tiletop = layout_icon_path .. "tiletopw.png"
+theme.layout_spiral  = layout_icon_path .. "spiralw.png"
+theme.layout_dwindle = layout_icon_path .. "dwindlew.png"
+theme.layout_cornernw = layout_icon_path .. "cornernww.png"
+theme.layout_cornerne = layout_icon_path .. "cornernew.png"
+theme.layout_cornersw = layout_icon_path .. "cornersww.png"
+theme.layout_cornerse = layout_icon_path .. "cornersew.png"
+
+-- Widget icons.
+local icon_path = theme_path .. "icons/"
+
+theme.widget_ac = icon_path .. "ac.png"
+theme.widget_battery = icon_path .. "battery.png"
+theme.widget_battery_low = icon_path .. "battery_low.png"
+theme.widget_battery_empty = icon_path .. "battery_empty.png"
+theme.widget_mem = icon_path .. "mem.png"
+theme.widget_cpu = icon_path .. "cpu.png"
+theme.widget_temp = icon_path .. "temp.png"
+theme.widget_net = icon_path .. "net.png"
+theme.widget_hdd = icon_path .. "hdd.png"
+theme.widget_music = icon_path .. "note.png"
+theme.widget_music_on = icon_path .. "note_on.png"
+theme.widget_vol = icon_path .. "vol.png"
+theme.widget_vol_low = icon_path .. "vol_low.png"
+theme.widget_vol_no = icon_path .. "vol_no.png"
+theme.widget_vol_mute = icon_path .. "vol_mute.png"
+theme.widget_mail = icon_path .. "mail.png"
+theme.widget_mail_on = icon_path .. "mail_on.png"
+theme.widget_task = icon_path .. "task.png"
 
 -- Generate Awesome icon:
 theme.awesome_icon = theme_assets.awesome_icon(
