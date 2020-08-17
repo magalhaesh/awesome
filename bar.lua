da3e2581fd3172d6a57ef223d2c44b79a52617c6
Henrique Magalhães <henrique.albertino@gmail.com>
2020-08-16 22:53:43 -0300

    Refactor custom wibar setup into a separate module

---
+++ bar.lua
@@ -0,0 +1,80 @@
+-- ===================================================================
+-- Sets up custom bar with tags, tasklist and widgets
+-- ===================================================================
+local gears = require("gears")
+local awful = require("awful")
+local wibox = require("wibox")
+local widgets = require("widgets")
+
+local bar = {}
+
+-- Create systray widget (holds systray icons for apps who provide them).
+local mysystray = wibox.widget.systray()
+
+-- Creates and configures custom wibar.
+function bar.setup(s, keys)
+    -- Create a promptbox for each screen.
+    s.mypromptbox = awful.widget.prompt()
+
+    -- Create an imagebox widget which will contain an icon indicating which
+    -- layout we're using.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+
+    s.mylayoutbox:buttons(gears.table.join(
+                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+
+    -- Create a taglist widget.
+    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.noempty, keys.taglist_buttons)
+
+    -- Create a tasklist widget.
+    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, keys.tasklist_buttons)
+
+    local left_sublayout = wibox.layout.fixed.horizontal()
+    local right_sublayout = wibox.layout.fixed.horizontal()
+
+    -- Left widgets.
+    left_sublayout:add(s.mytaglist)
+    left_sublayout:add(s.mypromptbox)
+
+    -- Right widgets.
+    if s.index == 2 then
+        mysystray.set_screen(s)
+        right_sublayout:add(mysystray)
+    end
+
+    local bg_color_1 = "#44475a"
+    local bg_color_2 = "#6272a4"
+
+    right_sublayout:add(widgets.get_left_arrow("alpha", bg_color_1))
+    right_sublayout:add(widgets.get_widget("mem", bg_color_1, 3, 4))
+
+    right_sublayout:add(widgets.get_left_arrow(bg_color_1, bg_color_2))
+    right_sublayout:add(widgets.get_widget("cpu", bg_color_2, 4, 4))
+
+    right_sublayout:add(widgets.get_left_arrow(bg_color_2, bg_color_1))
+    right_sublayout:add(widgets.get_widget("fs", bg_color_1, 3, 3))
+
+    right_sublayout:add(widgets.get_left_arrow(bg_color_1, bg_color_2))
+    right_sublayout:add(widgets.get_widget("net", bg_color_2, 3, 3))
+
+    right_sublayout:add(widgets.get_left_arrow(bg_color_2, bg_color_1))
+    right_sublayout:add(widgets.get_widget("clock", bg_color_1, 4, 8))
+
+    right_sublayout:add(widgets.get_left_arrow(bg_color_1, bg_color_2))
+    right_sublayout:add(widgets.wrap_widget(nil, s.mylayoutbox, bg_color_2, 0, 0))
+
+    -- Putting everything together in one box.
+    local layout = wibox.layout.align.horizontal()
+    layout:set_left(left_sublayout)
+    layout:set_middle(s.mytasklist)
+    layout:set_right(right_sublayout)
+
+    -- Create the wibar.
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 18 })
+    s.mywibox:set_widget(layout)
+end
+
+return bar
--- rc.lua
+++ rc.lua
@@ -1,23 +1,20 @@
 -- Standard awesome library
 local gears = require("gears")
 local awful = require("awful")
-
--- Widget and layout library
-local wibox = require("wibox")
-
--- Theme handling library
-local beautiful = require("beautiful")
-beautiful.init(gears.filesystem.get_configuration_dir() .. "/themes/wayfarer/theme.lua")
-
--- Notification library
 local naughty = require("naughty")
-local hotkeys_popup = require("awful.hotkeys_popup")
+local beautiful = require("beautiful")
+
+-- Initialize theme
+beautiful.init(
+    gears.filesystem.get_configuration_dir() ..
+    "/themes/wayfarer/theme.lua"
+)
 
 -- Helpers functions
 local helpers = require("helpers")
 
--- Custom Widgets
-local widgets = require("widgets")
+-- Custom Wibar
+local bar = require("bar")
 
 -- Set up root keys and buttons
 local keys = require("keys")
@@ -53,12 +50,6 @@ do
 end
 -- }}}
 
--- {{{ Variable definitions
--- This is used later as the default terminal and editor to run.
-terminal = "kitty"
-editor = os.getenv("EDITOR") or "vim"
-editor_cmd = terminal .. " -e " .. editor
-
 -- Table of layouts to cover with awful.layout.inc. Order matters.
 awful.layout.layouts = {
     awful.layout.suit.tile,
@@ -71,85 +62,20 @@ awful.layout.layouts = {
     awful.layout.suit.corner.ne,
     awful.layout.suit.corner.nw
 }
--- }}}
-
--- {{{ Wibar
--- Create systray widget (holds systray icons for apps who provide them).
-mysystray = wibox.widget.systray()
 
 -- Setup wiboxes, tags and wallpaper for each screen
 awful.screen.connect_for_each_screen(function(s)
     -- Wallpaper
     helpers.set_wallpaper(s)
 
-    -- Each screen has its own tag table.
+    -- Tag table
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9" }, s, awful.layout.layouts[1])
 
-    -- Create a promptbox for each screen
-    s.mypromptbox = awful.widget.prompt()
-    -- Create an imagebox widget which will contain an icon indicating which layout we're using.
-    -- We need one layoutbox per screen.
-    s.mylayoutbox = awful.widget.layoutbox(s)
-    s.mylayoutbox:buttons(gears.table.join(
-                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
-                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
-                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
-    -- Create a taglist widget
-    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.noempty, keys.taglist_buttons)
-
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, keys.tasklist_buttons)
-
-    -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s, height = 18 })
-
-    local left_sublayout = wibox.layout.fixed.horizontal()
-    local right_sublayout = wibox.layout.fixed.horizontal()
-    local layout = wibox.layout.align.horizontal()
-
-    -- Left widgets
-    left_sublayout:add(s.mytaglist)
-    left_sublayout:add(s.mypromptbox)
-
-    -- Right widgets
-    if s.index == 2 then
-        mysystray.set_screen(s)
-        right_sublayout:add(mysystray)
-    end
-
-    local bg_color_1 = "#44475a"
-    local bg_color_2 = "#6272a4"
-
-    right_sublayout:add(widgets.get_left_arrow("alpha", bg_color_1))
-    right_sublayout:add(widgets.get_widget("mem", bg_color_1, 3, 4))
-
-    right_sublayout:add(widgets.get_left_arrow(bg_color_1, bg_color_2))
-    right_sublayout:add(widgets.get_widget("cpu", bg_color_2, 4, 4))
-
-    right_sublayout:add(widgets.get_left_arrow(bg_color_2, bg_color_1))
-    right_sublayout:add(widgets.get_widget("fs", bg_color_1, 3, 3))
-
-    right_sublayout:add(widgets.get_left_arrow(bg_color_1, bg_color_2))
-    right_sublayout:add(widgets.get_widget("net", bg_color_2, 3, 3))
-
-    right_sublayout:add(widgets.get_left_arrow(bg_color_2, bg_color_1))
-    right_sublayout:add(widgets.get_widget("clock", bg_color_1, 4, 8))
-
-    right_sublayout:add(widgets.get_left_arrow(bg_color_1, bg_color_2))
-    right_sublayout:add(widgets.wrap_widget(nil, s.mylayoutbox, bg_color_2, 0, 0))
-
-    -- Putting everything together
-    layout:set_left(left_sublayout)
-    layout:set_middle(s.mytasklist)
-    layout:set_right(right_sublayout)
-
-    s.mywibox:set_widget(layout)
+    -- Create bar
+    bar.setup(s, keys)
 end)
--- }}}
 
 -- {{{ Signals
-
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
 screen.connect_signal("property::geometry", helpers.set_wallpaper)
 
